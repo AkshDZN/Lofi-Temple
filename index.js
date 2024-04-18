c8ea50a31e3f91fbd73b463dc56c535b5a13d64a
AkshDZN <129135009+AkshDZN@users.noreply.github.com>
2024-04-18 13:29:15 +0530

    fixed mixer appearance across multiple devices

--- index.js
+++ index.js
@@ -118,6 +118,8 @@ function askTime() {
 
 // theme switcher 
 const r = document.querySelector(":root");
+const switcher = document.getElementById("sr");
+const switcher2 = document.getElementById("sl");
 
 //blaze
 const th1bg = "url('assets/blaze.png')";
@@ -125,17 +127,26 @@ const th1dash = "rgba(131, 43, 24, 0.26)";
 const th1dashHover = "rgba(131, 43, 24, 0.5)";
 const th1dstroke = "0.5px solid rgba(255, 187, 173, 0.26)";
 
+const th1accent = "rgba(255, 74, 0, 1)";
+const th1secondary = "rgba(255, 196, 172, 1)";
+
 // play
 const th2bg = "url('assets/play.png')";
 const th2dash = "rgba(24, 28, 131, 0.26)";
 const th2dashHover = "rgba(24, 28, 131, 0.5)";
 const th2dstroke = "0.5px solid rgba(179, 173, 255, 0.26)";
 
+const th2accent = "rgba(47, 104, 255, 1)";
+const th2secondary = "rgba(172, 195, 255, 1)";
+
 // luxury
 const th3bg = "url('assets/luxury.png')";
 const th3dash = "rgba(24, 28, 131, 0.26)";
-const th3dashHover = "rgba(24, 28, 131, 0.5)";
+const th3dashHover = "rgba(24, 28, 131, 0.5)";	
 const th3dstroke = "0.5px solid rgba(179, 173, 255, 0.26)";
+
+const th3accent = "rgba(47, 104, 255, 1)";
+const th3secondary = "rgba(172, 195, 255, 1)";
 
 //dreams
 const th4bg = "url('assets/dreams.png')";
@@ -143,11 +154,17 @@ const th4dash = "rgba(131, 43, 24, 0.26)";
 const th4dashHover = "rgba(131, 43, 24, 0.5)";
 const th4dstroke = "0.5px solid rgba(255, 187, 173, 0.26)";
 
+const th4accent = "rgba(47, 104, 255, 1)";
+const th4secondary = "rgba(172, 195, 255, 1)";
+
 //memories
 const th5bg = "url('assets/memories.png')";
 const th5dash = "rgba(131, 43, 24, 0.26)";
 const th5dashHover = "rgba(131, 43, 24, 0.5)";
 const th5dstroke = "0.5px solid rgba(255, 187, 173, 0.26)";
+
+const th5accent = "rgba(255, 144, 98, 1)";
+const th5secondary = "rgba(255, 196, 172, 1)";
 
 function theme(thno) {
 	if (thno === 1) {
@@ -155,26 +172,51 @@ function theme(thno) {
 		r.style.setProperty("--dashBg" , th1dash);
 		r.style.setProperty("--dashBgHover" , th1dashHover);
 		r.style.setProperty("--dashStroke" , th1dstroke);
+
+		r.style.setProperty("--mixerAccent", th1accent);
+		r.style.setProperty("--mixerSecondary", th1secondary);
+		switcher.src = "assets/icons/switcherth1.svg"
+		switcher2.src = "assets/icons/switcherth1.svg"
 	} else if (thno === 2) {
 		r.style.setProperty("--bg" , th2bg);
 		r.style.setProperty("--dashBg" , th2dash);
 		r.style.setProperty("--dashBgHover" , th2dashHover);
 		r.style.setProperty("--dashStroke" , th2dstroke);
+
+		r.style.setProperty("--mixerAccent", th2accent);
+		r.style.setProperty("--mixerSecondary", th2secondary);
+		switcher.src = "assets/icons/switcherth234.svg";
+		switcher2.src = "assets/icons/switcherth234.svg";
 	} else if (thno === 3) {
 		r.style.setProperty("--bg" , th3bg);
 		r.style.setProperty("--dashBg" , th3dash);
 		r.style.setProperty("--dashBgHover" , th3dashHover);
 		r.style.setProperty("--dashStroke" , th3dstroke);
+
+		r.style.setProperty("--mixerAccent", th3accent);
+		r.style.setProperty("--mixerSecondary", th3secondary);
+		switcher.src = "assets/icons/switcherth234.svg";
+		switcher2.src = "assets/icons/switcherth234.svg";
 	} else if (thno === 4) {
 		r.style.setProperty("--bg" , th4bg);
 		r.style.setProperty("--dashBg" , th4dash);
 		r.style.setProperty("--dashBgHover" , th4dashHover);
 		r.style.setProperty("--dashStroke" , th4dstroke);
+
+		r.style.setProperty("--mixerAccent", th4accent);
+		r.style.setProperty("--mixerSecondary", th4secondary);
+		switcher.src = "assets/icons/switcherth234.svg";
+		switcher2.src = "assets/icons/switcherth234.svg";
 	} else if (thno === 5) {
 		r.style.setProperty("--bg" , th5bg);
 		r.style.setProperty("--dashBg" , th5dash);
 		r.style.setProperty("--dashBgHover" , th5dashHover);
 		r.style.setProperty("--dashStroke" , th5dstroke);
+
+		r.style.setProperty("--mixerAccent", th5accent);
+		r.style.setProperty("--mixerSecondary", th5secondary);
+		switcher.src = "assets/icons/switcherth5.svg";
+		switcher2.src = "assets/icons/switcherth5.svg";
 	}
 }
 
@@ -183,6 +225,7 @@ $("#slider").roundSlider({
     width: 22,
     radius: 130,
     value: 0,
+	showTooltip: false,
     mouseScrollAction: true,
     circleShape: "pie",
     startAngle: 315,
@@ -198,11 +241,13 @@ const soundimage = document.querySelector(".soundimage");
 let soundState = 0;
 function soundMenu() {
 	if (soundState === 0) {
-		soundmixer.style.display = "flex";
+		// soundmixer.style.display = "flex";
+		soundmixer.style.scale = "1";
 
 		soundState = 1;
 	} else if (soundState === 1) {
-		soundmixer.style.display = "none";
+		// soundmixer.style.display = "none";
+		soundmixer.style.scale = "0";
 
 		soundState = 0;
 	}
